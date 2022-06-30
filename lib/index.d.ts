366935a06438973e30473ea0e264e4b0f9ca6d1c
Amruta Khot <akhot@skillz.com>
2022-06-30 16:52:39 -0700

    fixed imports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,9 @@
 import { AuthenticationService } from './services/sdk/AuthenticationService';
 import { LoginBonusService } from './services/sdk/loginBonusService';
-import SkillzSDKAPI, { Configuration } from './services/sdk/skillzSDKAPI';
+import { Configuration } from './services/sdk/skillzSDKAPI';
 import { UserCurrencyService } from './services/sdk/userCurrencyService';
 import { UserManagementService } from './services/sdk/userManagementService';
-declare const _default: {
-    AuthenticationService: typeof AuthenticationService;
-    LoginBonusService: typeof LoginBonusService;
-    SkillzSDKAPI: typeof SkillzSDKAPI;
-    UserCurrencyService: typeof UserCurrencyService;
-    UserManagementService: typeof UserManagementService;
-};
-export default _default;
+import { PortalsAuthenticationService } from './services/portals/portalsAuthenticationService';
+import { PlayerDetailsService } from './services/portals/playerDetailsService';
+export { AuthenticationService, LoginBonusService, UserCurrencyService, UserManagementService, PortalsAuthenticationService, PlayerDetailsService };
 export type { Configuration };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import {AuthenticationService} from './services/sdk/AuthenticationService';
+import { AuthenticationService } from './services/sdk/AuthenticationService';
 import { LoginBonusService } from './services/sdk/loginBonusService';
-import SkillzSDKAPI, { Configuration } from './services/sdk/skillzSDKAPI';
+import { Configuration } from './services/sdk/skillzSDKAPI';
 import { UserCurrencyService } from './services/sdk/userCurrencyService';
 import { UserManagementService } from './services/sdk/userManagementService';
+import { PortalsAuthenticationService } from './services/portals/portalsAuthenticationService';
+import { PlayerDetailsService } from './services/portals/playerDetailsService';
 
-export default{AuthenticationService, LoginBonusService, SkillzSDKAPI, UserCurrencyService, UserManagementService};
-export type {Configuration};
+
+export { AuthenticationService, LoginBonusService, UserCurrencyService, UserManagementService, PortalsAuthenticationService, PlayerDetailsService };
+export type { Configuration };
