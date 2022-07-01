c4d56a18b384ba6c3c3ac75711860d31303b9c81
Amruta Khot <akhot@skillz.com>
2022-06-30 21:18:16 -0700

    added more imports as needed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import { AuthenticationService } from './services/sdk/AuthenticationService';
+import { AuthenticationService, Credentials } from './services/sdk/AuthenticationService';
 import { LoginBonusService } from './services/sdk/loginBonusService';
 import SkillzSDKAPI, { Configuration } from './services/sdk/skillzSDKAPI';
 import SkillzPortalsAPI, { PortalsConfiguration } from './services/portals/skillzPortalsAPI';
 import { UserCurrencyService } from './services/sdk/userCurrencyService';
-import { UserManagementService } from './services/sdk/userManagementService';
+import { UserManagementService, User } from './services/sdk/userManagementService';
 import { PortalsAuthenticationService } from './services/portals/portalsAuthenticationService';
-import { PlayerDetailsService } from './services/portals/playerDetailsService';
+import { PlayerDetailsService, BalanceRequest, UpdateBalanceUserInfoResponse, UpdateTicketzBalanceRequest, UpdateTicketzUserInfoResponse, GetTicketzBalanceResponse, GetTicketzBalanceRequest, GetUserSegmentMembershipRequest } from './services/portals/playerDetailsService';
+import FingerprintService from './services/internal/fingerprintService';
 
+export { 
+    AuthenticationService, 
+    LoginBonusService, 
+    UserCurrencyService,
+    UserManagementService, 
+    PortalsAuthenticationService, 
+    SkillzPortalsAPI, 
+    SkillzSDKAPI, 
+    User, 
+    PlayerDetailsService, 
+    BalanceRequest, 
+    UpdateBalanceUserInfoResponse, 
+    UpdateTicketzBalanceRequest, 
+    UpdateTicketzUserInfoResponse, 
+    GetTicketzBalanceResponse, 
+    GetTicketzBalanceRequest, 
+    Credentials, 
+    GetUserSegmentMembershipRequest, 
+    FingerprintService
+};
 
-export { AuthenticationService, LoginBonusService, UserCurrencyService, UserManagementService, PortalsAuthenticationService, PlayerDetailsService, SkillzPortalsAPI, SkillzSDKAPI };
 export type { Configuration, PortalsConfiguration };
