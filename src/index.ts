44ab269a738513d6aa6387d22fc140bb09a07f7b
Amruta Khot <akhot@skillz.com>
2022-06-30 17:11:17 -0700

    importing skillz sdk and portal api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { AuthenticationService } from './services/sdk/AuthenticationService';
 import { LoginBonusService } from './services/sdk/loginBonusService';
-import { Configuration } from './services/sdk/skillzSDKAPI';
+import SkillzSDKAPI, { Configuration } from './services/sdk/skillzSDKAPI';
+import SkillzPortalsAPI, { PortalsConfiguration } from './services/portals/skillzPortalsAPI';
 import { UserCurrencyService } from './services/sdk/userCurrencyService';
 import { UserManagementService } from './services/sdk/userManagementService';
 import { PortalsAuthenticationService } from './services/portals/portalsAuthenticationService';
 import { PlayerDetailsService } from './services/portals/playerDetailsService';
 
 
-export { AuthenticationService, LoginBonusService, UserCurrencyService, UserManagementService, PortalsAuthenticationService, PlayerDetailsService };
-export type { Configuration };
+export { AuthenticationService, LoginBonusService, UserCurrencyService, UserManagementService, PortalsAuthenticationService, PlayerDetailsService, SkillzPortalsAPI, SkillzSDKAPI };
+export type { Configuration, PortalsConfiguration };
